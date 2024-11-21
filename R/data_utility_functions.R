bef71f7085f02b08c7879d588b40c01c32073482
David Cardinal <github@cardinalphoto.com>
2024-11-20 22:33:54 -0800

    Added roxygen function comments

--- R/data_utility_functions.R
+++ R/data_utility_functions.R
@@ -2,6 +2,8 @@
 #'
 #'
 
+#' @param our_dataset Already loaded dataset
+#' @return Properties of the dataset
 get_dataset_properties <- function(our_dataset) {
   return(our_dataset$properties)
 }
@@ -9,6 +11,9 @@ get_dataset_properties <- function(our_dataset) {
 # List all the datasets to which you have access in a particular organization.
 # This is a way to get the update time and/or version of a dataset
 # to compare with a previously downloaded copy
+
+#' @param org_name Return datasets belonging to this Organization
+#' @return List of datasets belonging to the organization
 list_organization_datasets <- function(org_name = 'levante') {
   org <- redivis::organization(org_name)
   datasets <- org$list_datasets()
@@ -24,6 +29,8 @@ list_organization_datasets <- function(org_name = 'levante') {
 # This is a way to get the update time and/or version of a dataset
 # to compare with a previously downloaded copy
 # (Can we get the username for the current user ourselves?)
+#' @param user_name List all datasets owned by this user
+#' @return list of datasets owned by the user
 list_user_datasets <- function(user_name) {
 
   # Datasets per user
@@ -38,6 +45,9 @@ list_user_datasets <- function(user_name) {
 }
 
 # pick a specific dataset out of the list of datasets provided
+#' @param dataset_list List of datasets to search.
+#' @param dataset_name Name of dataset we are looking for.
+#' @return The dataset we found.
 find_dataset <- function(dataset_list, dataset_name) {
   found_dataset <- dataset_list[sapply(dataset_list, +
                                          function(x) x$name == dataset_name)]
