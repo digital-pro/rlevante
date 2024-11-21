0bde4076d798cfa79370cef7752f6a48339f7feb
David Cardinal <github@cardinalphoto.com>
2024-11-20 21:09:34 -0800

    caching comments

--- R/data_caching_functions.R
+++ R/data_caching_functions.R
@@ -2,9 +2,16 @@
 #'
 #'
 
+# Not sure if cache should be per project or per user
+# For now set the default to be under the current project
 dataset_cache_dir <- "./dataset_cache/"
 
 # Save an in memory dataset to a cache location
+# We also want to save at least the last_updated time for re-loading
+# so we save all the properties in case we need others
+# Annoyingly, the dataset doesn't include its name or its
+# properties (like last_updated). Fixing that might require
+# some modification of get_datasets so we know what we got.
 cache_dataset <- function(our_dataset, cache_dir = dataset_cache_dir) {
   save(our_dataset, paste(cache_dir, our_dataset$name))
   #return(success_flag)
